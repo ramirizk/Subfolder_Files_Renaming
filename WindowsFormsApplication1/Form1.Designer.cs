388c15a43205b31168ee60c7ff547caa46849673
Rami Rizk <ramirizk87@gmail.com>
2016-10-27 16:38:48 -0400

    Intial Commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 626);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rename Selected Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(810, 589);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 626);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(381, 624);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(812, 649);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private string dirOld;
+        private string dirNew;
+        private string dirPath;
+        private string origPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog( );
+            FBD.Description = "Select Subject that has folders to be renamed";
+
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                
+                origPath = FBD.SelectedPath;
+                listBox1.Items.Clear();
+                string[] files = Directory.GetFiles(FBD.SelectedPath);
+                string[] dirs = Directory.GetDirectories(FBD.SelectedPath);
+                foreach (string dir in dirs)
+                {
+                    listBox1.Items.Add(dir);
+                }
+            } else
+            {
+                Application.Exit();
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int x = listBox1.SelectedIndices.Count - 1; x >= 0; x--)
+            {
+                dirOld = listBox1.SelectedItems[x].ToString();
+                dirPath= Directory.GetParent(dirOld).ToString();
+                dirNew = dirPath + @"\" + Path.GetFileName(dirOld.TrimEnd(Path.DirectorySeparatorChar));
+                string ReplaceDir = Form1.ShowDialog("What is new name for Folder?", "Changing " + dirNew + " to New",dirNew);
+                Directory.CreateDirectory(dirPath + "\\" + ReplaceDir);
+                Directory.Move(dirOld + "\\Configuration", dirPath + "\\" + ReplaceDir + "\\Configuration");
+                DirectoryInfo hdSearch = new DirectoryInfo(dirOld + @"\Data");
+                FileInfo[] filesInDir = hdSearch.GetFiles("*" + Path.GetFileName(dirOld.TrimEnd(Path.DirectorySeparatorChar)) + "*");
+                foreach(FileInfo foundFile in filesInDir)
+                {
+                    string newName = hdSearch.FullName + @"\" + foundFile.Name.Replace(Path.GetFileName(dirOld.TrimEnd(Path.DirectorySeparatorChar)), ReplaceDir);
+                    string oldName=foundFile.FullName;
+                    File.Move(oldName,newName);
+                }
+                
+
+                Directory.Move(dirOld + "\\Data", dirPath+ "\\" + ReplaceDir + @"\Data");
+                Directory.Delete(dirOld);
+                
+            }
+
+        }
+
+        public static string ShowDialog(string text, string caption, string dirNew)
+        {
+            Form prompt = new Form()
+            {
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen,
+                AutoSize=true
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, AutoSize = true };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400, Text= Path.GetFileName(dirNew.TrimEnd(Path.DirectorySeparatorChar))};
+            Button confirmation = new Button() { Text = "ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+            
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+
+        }
+    
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string[] files = Directory.GetFiles(origPath);
+            string[] dirs = Directory.GetDirectories(origPath);
+            foreach (string dir in dirs)
+            {
+                listBox1.Items.Add(dir);
+            }
+        }
+    }
+    }
+
